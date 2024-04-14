87469bf39e3890f9c1e26cda9926d6afb72a813e
Ahmad Kabalan <ag.kabalan@gmail.com>
2024-04-14 18:33:17 +0300

    Refactored login service

--- src/main/java/com/edusenior/project/services/LoginServiceImpl.java
+++ src/main/java/com/edusenior/project/services/LoginServiceImpl.java
@@ -18,12 +18,15 @@ import java.sql.Timestamp;
 
 @Service
 public class LoginServiceImpl implements LoginService {
+    // 1 Hour
+    private final long timeoutInterval = 3600000;
+
     private BcryptPasswordEncoder encoder;
     private JWTManager jwtManager;
     private CredentialsDAO credentialsDAO;
 
     @Autowired
-    public LoginServiceImpl(JWTManager jwtManager, BcryptPasswordEncoder encoder, TeacherDAO teacherDAO, StudentDAO studentDAO,CredentialsDAO credentialsDAO) {
+    public LoginServiceImpl(JWTManager jwtManager, BcryptPasswordEncoder encoder,CredentialsDAO credentialsDAO) {
         this.jwtManager = jwtManager;
         this.encoder = encoder;
         this.credentialsDAO = credentialsDAO;
@@ -41,28 +44,39 @@ public class LoginServiceImpl implements LoginService {
         if (credentials == null){
             throw new LoginException("Invalid credentials");
         }
-        String id = credentials.getId();
-
-
-        Timestamp lockout = credentials.getLockout();
-        if (lockout == null){
-            throw new LoginException("Error logging in, Contact System Admin");
-        }
-        if (lockout.after(new Timestamp(System.currentTimeMillis()))){
+        if(isLockedOut(credentials)){
             throw new LoginException("You are not allowed to login at this time");
         }
+        if (!validatePasswordAndUpdateFailures(credentials,loginDTO)){
+            throw new LoginException("Invalid Credentials");
+        }
+        else{
+            resetFailedAttempts(credentials);
+        }
 
+        final String id = credentials.getId();
+        return new JwtDTO(jwtManager.generateToken(id, role));
+    }
+
+    private void resetFailedAttempts(Credentials credentials) {
+        credentials.setFailed(0);
+        credentialsDAO.persistChange(credentials);
+    }
+    private boolean validatePasswordAndUpdateFailures(Credentials credentials, LoginDTO loginDTO)  {
         if (!(encoder.passwordEncoder().matches(loginDTO.getPassword(), credentials.getHash()))) {
             int failed = credentials.addToFailed();
             if (failed >=10){
                 credentials.setFailed(0);
-                credentials.setLockout(new Timestamp(System.currentTimeMillis() + 3600000));
+                credentials.setLockout(new Timestamp(System.currentTimeMillis() + timeoutInterval));
             }
             credentialsDAO.persistChange(credentials);
-            throw new LoginException("Invalid credentials");
+            return false;
         }
-
-        return new JwtDTO(jwtManager.generateToken(id, role));
+        return true;
+    }
+    private boolean isLockedOut(Credentials credentials) throws LoginException {
+        final Timestamp lockout = credentials.getLockout();
+        return lockout != null && lockout.after(new Timestamp(System.currentTimeMillis()));
     }
 }
 
